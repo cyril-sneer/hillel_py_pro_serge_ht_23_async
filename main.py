cd4b8775fe804caf35ab64889700bb28f22e064d
Sergey Shapiro <syrill.snir@gmail.com>
2023-09-11 00:58:06 +0200

    get_forecast_open_meteo()

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 https://github.com/public-apis/public-apis#weather
 """
-import requests
+import requests, datetime
 
 from pprint import pprint
 
@@ -34,17 +34,43 @@ def get_forecast_7timer():
         data = result.json()
 
         temp_values_for_next_24 = []
-        for record in data.get('dataseries')[:8]:
+        # Сервис дает прогноз на 8 дней с шагом 3 часа, то есть 8 измерений в сутки.
+        # Поэтому для прогноза на ближайшие 7 дней, берем первые 56 значений
+        for record in data.get('dataseries')[:56]:
             temp_values_for_next_24.append(record.get('temp2m'))
 
         min_temp = min(temp_values_for_next_24)
         max_temp = max(temp_values_for_next_24)
         avg_temp = sum(temp_values_for_next_24)/len(temp_values_for_next_24)
-        print(temp_values_for_next_24, min_temp, max_temp, avg_temp)
+        print(temp_values_for_next_24, min_temp, max_temp, avg_temp, sep='\n')
+
+def get_forecast_open_meteo():
+    """
+    https://open-meteo.com/
+    :return:
+    """
+    url = 'https://api.open-meteo.com/v1/forecast'
+    query_params = {
+        'latitude': MyCity.lat,
+        'longitude': MyCity.lon,
+        'hourly': 'temperature_2m',
+        'forecast_days': 7,
+    }
+
+    result = requests.get(url=url, params=query_params)
+    if result.status_code == 200:
+        data = result.json()
+        temp_values_for_next_24 = data.get('hourly').get('temperature_2m')
+
+        min_temp = min(temp_values_for_next_24)
+        max_temp = max(temp_values_for_next_24)
+        avg_temp = sum(temp_values_for_next_24) / len(temp_values_for_next_24)
+        print(temp_values_for_next_24, min_temp, max_temp, avg_temp, sep='\n')
 
 
 def main():
     get_forecast_7timer()
+    get_forecast_open_meteo()
 
 
 if __name__ == '__main__':
