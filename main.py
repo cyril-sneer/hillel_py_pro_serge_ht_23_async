fd5f092a43f707ffbf6ed15151e9d68dafffc6c8
Sergey Shapiro <syrill.snir@gmail.com>
2023-09-11 03:01:46 +0200

    get_forecast_open_meteo()

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 """
 https://github.com/public-apis/public-apis#weather
 """
-import requests, datetime
+import requests
 
 from pprint import pprint
 
 
 class MyCity:
-    name = 'Lille'
     lon = 3.14
     lat = 50.67
 
 
-def get_forecast_7timer():
+def get_forecast_7timer() -> list:
     """
     http://www.7timer.info/doc.php?lang=en
 
@@ -28,23 +27,22 @@ def get_forecast_7timer():
         "tzshift": 0,
     }
 
-    result = requests.get(url=url, params=query_params)
+    response = requests.get(url=url, params=query_params)
 
-    if result.status_code == 200:
-        data = result.json()
+    if response.status_code == 200:
+        forecast = response.json()
 
-        temp_values_for_next_24 = []
+        temp_values_for_next_5_days = []
         # Сервис дает прогноз на 8 дней с шагом 3 часа, то есть 8 измерений в сутки.
-        # Поэтому для прогноза на ближайшие 7 дней, берем первые 56 значений
-        for record in data.get('dataseries')[:56]:
-            temp_values_for_next_24.append(record.get('temp2m'))
+        # Поэтому для прогноза на ближайшие 5 дней, берем первые 40 значений
+        for record in forecast.get('dataseries')[:40]:
+            temp_values_for_next_5_days.append(record.get('temp2m'))
+        return temp_values_for_next_5_days
+    else:
+        return []
 
-        min_temp = min(temp_values_for_next_24)
-        max_temp = max(temp_values_for_next_24)
-        avg_temp = sum(temp_values_for_next_24)/len(temp_values_for_next_24)
-        print(temp_values_for_next_24, min_temp, max_temp, avg_temp, sep='\n')
 
-def get_forecast_open_meteo():
+def get_forecast_open_meteo() -> list:
     """
     https://open-meteo.com/
     :return:
@@ -54,23 +52,59 @@ def get_forecast_open_meteo():
         'latitude': MyCity.lat,
         'longitude': MyCity.lon,
         'hourly': 'temperature_2m',
-        'forecast_days': 7,
+        'forecast_days': 5,
     }
 
-    result = requests.get(url=url, params=query_params)
-    if result.status_code == 200:
-        data = result.json()
-        temp_values_for_next_24 = data.get('hourly').get('temperature_2m')
+    response = requests.get(url=url, params=query_params)
+    if response.status_code == 200:
+        forecast = response.json()
+        temp_values_for_next_5_days = forecast.get('hourly').get('temperature_2m')
+        return temp_values_for_next_5_days
+    else:
+        return []
 
-        min_temp = min(temp_values_for_next_24)
-        max_temp = max(temp_values_for_next_24)
-        avg_temp = sum(temp_values_for_next_24) / len(temp_values_for_next_24)
-        print(temp_values_for_next_24, min_temp, max_temp, avg_temp, sep='\n')
 
+def get_forecast_accu_weather() -> list:
+    """
+    https://developer.accuweather.com/apis
+    vwKlaeDmagfRGAycHzvxwInGupggm0tF
+    :return:
+    """
+    api_key = 'vwKlaeDmagfRGAycHzvxwInGupggm0tF'
+
+    # Получить код местоположения
+    url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
+    query_params = {
+        'apikey': api_key,
+        'q': f'{MyCity.lat},{MyCity.lon}'
+    }
+
+    response = requests.get(url=url, params=query_params)
+    if response.status_code == 200:
+        location_key = response.json().get('Key')
+    else:
+        return []
+
+    url = f'http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}'
+    query_params = {
+        'apikey': api_key,
+        'metric': 'true'
+    }
+    response = requests.get(url=url, params=query_params)
+    if response.status_code == 200:
+        forecast = response.json()
+        temp_values_for_next_5_days = []
+        for _ in forecast.get('DailyForecasts'):
+            temp_values_for_next_5_days.append(_.get('Temperature').get('Maximum').get('Value'))
+            temp_values_for_next_5_days.append(_.get('Temperature').get('Minimum').get('Value'))
+        return temp_values_for_next_5_days
+    else:
+        return []
 
 def main():
-    get_forecast_7timer()
-    get_forecast_open_meteo()
+    print(get_forecast_7timer())
+    print(get_forecast_open_meteo())
+    print(get_forecast_accu_weather())
 
 
 if __name__ == '__main__':
